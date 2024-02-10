1ba8f088229e80e78470e9b1aeb48e283911f179
youssefmmagdy <125548527+youssefmmagdy@users.noreply.github.com>
2024-02-10 06:11:21 +0200

    Add files via upload

---
+++ functions_procedures.sql
@@ -0,0 +1,467 @@
+﻿--4.1.a
+create procedure PatientRegister 
+(@patient_id int,
+@patient_name varchar(20),
+@ehr varchar(30),
+@contact varchar(30),
+@allergies varchar(30),
+@diseases varchar(30),
+@vaccines varchar(30),
+@drugs varchar(30),
+@exam_result varchar(30),
+@password varchar(20),
+@nurse_id int,
+@ins_company varchar(20),
+@nurse_id int
+) as
+insert into Patient values(@patient_id,@patient_name,
+@ehr,
+@contact,
+@allergies,
+@diseases,
+@vaccines,
+@drugs,
+@exam_result,
+@password,
+@ins_company,
+@nurse_id);
+go--to seperate batches
+--4.1.b
+CREATE PROCEDURE DoctorRegister (
+    @doctor_id INT,
+    @working_hours INT,
+    @working_days INT,
+    @doctor_name VARCHAR(15),
+    @specialization VARCHAR(20),
+    @hospital_name VARCHAR(20),
+    @password varchar(20)
+)
+AS
+BEGIN
+    INSERT INTO Doctor 
+    VALUES (
+        @doctor_id,
+        @working_hours,
+        @working_days,
+        @doctor_name,
+        @specialization,
+        @hospital_name,
+        @password
+    );
+END;
+go
+--4.1.c
+CREATE PROCEDURE PharmacistRegister (
+    @ph_id int,
+    @pharmacy_name varchar(15),
+    @startDate date,
+    @password varchar(20),
+    @pharmacist_name varchar(20)
+)
+AS
+BEGIN
+    INSERT INTO Pharmacist
+    VALUES (
+        @ph_id,
+        @ph_name,
+        @startDate,
+        @password,
+        @pharmacist_name
+    );
+END;
+go
+--4.1.d
+CREATE PROCEDURE NurseRegister (
+    @nurse_id int,
+    @first_name varchar(15),
+    @middle_name varchar(15),
+    @last_name varchar(15),
+    @nurst_shift varchar(15),
+    @hospital_name varchar(10),
+    @password varchar(20)
+)
+AS
+BEGIN
+    INSERT INTO Nurse
+    VALUES (
+        @nurse_id,
+        @first_name,
+        @middle_name,
+        @last_name,
+        @nurst_shift,
+        @hospital_name,
+        @passwordw
+    );
+END;
+go
+--4.2.a
+create function userLogin (@id int,@password varchar(20))
+returns bit as
+begin
+declare @res bit
+if(exists (select userId,pass_word from User_table 
+where userId = @id and @password = pass_word))set @res = 1
+else set @res = 0
+return @res
+end;
+
+go
+--4.2.b
+CREATE PROCEDURE addMobile (
+	@ID int, 
+	@mobile_number varchar(20) 
+)
+AS 
+BEGIN
+	INSERT INTO Doctor_Numbers
+	VALUES(
+		@ID , 
+		@mobile_number 
+		);
+END;
+go
+--4.3.a
+CREATE PROCEDURE PatientList
+AS
+BEGIN
+    SELECT * FROM Patient;
+END;
+go
+--4.3.b
+CREATE PROCEDURE DoctorsList
+AS
+BEGIN
+    SELECT * FROM Doctor;
+END;
+go
+--4.3.c
+CREATE PROCEDURE PharmacistList
+AS
+BEGIN
+    SELECT * FROM Pharmacist;
+END;
+go
+--4.3.d
+create function AdminViewPatientProfile (@patientId int) returns table as
+RETURN (
+    SELECT *
+    FROM Patient p
+    where @patientId = p.patient_id
+);
+go
+--4.3.e
+create function AdminAddPatientInfo (@patientId int,
+@ehr varchar(30),
+@contact varchar(30),
+@allergies varchar(30),
+@diseases varchar(30),
+@vaccines varchar(30),
+@drugs varchar(30),
+@exam_result varchar(30),
+@nurse_id int,
+@ins_company varchar(20)
+) returns bit as
+--insert a new entry in table patient,I assumed patient's info contains no patient_id
+begin
+insert into Patient values(@patient_id,
+@ehr,
+@contact,
+@allergies,
+@diseases,
+@vaccines,
+@drugs,
+@exam_result,
+@nurse_id,
+@ins_company
+);
+return 1;
+end;
+go
+--4.3.f
+create function AdminUpdatePatientInfo (@patientId int,
+@ehr varchar(30),
+@contact varchar(30),
+@allergies varchar(30),
+@diseases varchar(30),
+@vaccines varchar(30),
+@drugs varchar(30),
+@exam_result varchar(30),
+@nurse_id int,
+@ins_company varchar(20)
+) returns bit as
+--insert a new entry in table patient,I assumed patient's info contains no patient_id
+begin
+update Patient set ehr = @ehr,
+contact = @contact,allergies=@allergies,diseases=@diseases,
+vaccines=@vaccines,drugs=@drugs,
+exam_result=@exam_result,nurse_id=@nurse_id,ins_company=@ins_company
+where patient_id = @patient_id
+return 1;
+end;
+go
+--4.3.g
+CREATE PROCEDURE AdminViewPatientAndHospitalPerDoc
+    @doctor_name varchar(15),
+    @PatientName varchar(20),
+    @hospital_name varchar(20)
+AS
+BEGIN
+    SELECT D.doctor_name, P.patient_name, D.hospital_name    
+    FROM Appointment A
+    INNER JOIN Doctor D ON A.doctor_id = D.doctor_id
+    INNER JOIN Patient P ON A.patient_id = P.patient_id
+    WHERE P.patient_name = @PatientName AND D.doctor_name = @doctor_name AND D.hospital_name = @hospital_name
+    GROUP BY D.doctor_id ;
+END;go
+--4.3.h
+CREATE PROCEDURE AdminViewAllPatientsAndMeds
+    @PatientName varchar(20),
+    @medication_type varchar(20)
+AS
+BEGIN
+    SELECT P.patient_name, M.medication_type    
+    FROM Takes_Medication T
+    INNER JOIN Patient P ON T.patient_id = P.patient_id
+    INNER JOIN Medication M ON M.medication_id = T.medication_id
+    WHERE P.patient_name = @PatientName AND M.medication_type = @medication_type
+    GROUP BY patient_id ;
+END;
+go
+--4.3.i
+CREATE PROCEDURE AdminViewPatientsAndMeds
+    @PatientID INT
+AS
+BEGIN
+    SELECT P.patient_name, M.mediaction_type
+    FROM Patient AS P
+    INNER JOIN Takes_Medication AS TM ON P.patient_id = TM.patient_id
+    INNER JOIN Medication AS M ON TM.medication_id = M.medication_id
+    WHERE P.patient_id = @PatientID;
+END;
+go
+--4.3.j
+
+CREATE PROCEDURE AdminViewTreatmentPerDocAndPatient
+     @TreatmentID INT
+AS
+BEGIN
+    SELECT  treatment_id
+	FROM  Writes_Treatmnet
+	WHERE treatment_id = @TreatmentID
+END;
+go
+--4.3.k
+CREATE PROCEDURE AdminViewLabPerDocAndPatient
+     @LabTest INT
+AS
+BEGIN
+    SELECT  labtest_id
+	FROM  Labtest
+	WHERE labtest_id = @LabTest
+END;
+go
+--4.3.l
+CREATE PROCEDURE AdminViewPresPerDocAndPatient
+     @prescription_id INT
+AS
+BEGIN
+    SELECT  prescription_id
+	FROM  Writes_Prescription
+	WHERE prescription_id = @prescription_id
+END;
+go
+--4.3.m
+CREATE PROCEDURE AdminViewAllPatientsAndMeds
+     @ph_name varchar(20) , @prescription_id varchar(20)
+AS
+BEGIN
+    SELECT  PH.ph_name , PCP.prescription_id
+	FROM  Pharmacist PH INNER JOIN Pharmacist_checks_Prescription PCP ON PH.ph_id = PH.ph_id	  
+	WHERE PH.ph_name = @ph_name and PCP.prescription_id = @prescription_id
+END;
+go
+--4.3.n
+create function AdminViewPharmacistProfile (@PharmacistID int) returns table as
+return(select * from Pharmacist ph where ph.ph_id = @PharmacistID);
+go
+
+--4.3.o
+create function AdminViewNurse() returns table as
+return(
+select n.first_name,n.middle_name,n.last_name,n.nurse_shift,n.hospital_name,p.patient_name
+from Nurse n inner join Patient p on n.nurse_id=p.nurse_id);
+go
+--4.3.p
+create function AdminViewHospital() returns table as
+return(
+select h.hospital_name,h.hospital_location,h.email,p.patient_name
+from Hospital h inner join Hospital_Manages hm 
+on h.hospital_name = hm.hospital_name inner join Patient p
+on hm.patient_id = p.patient_id
+);
+go
+--4.3.q
+CREATE PROCEDURE AdminViewAmb
+     @service_type varchar(20) , @hospital_name varchar(20)
+AS
+BEGIN
+    SELECT  ES.service_type , SC.hospital_name
+	FROM  Services_Call SC INNER JOIN Emergency_Services ES ON SC.service_id = ES.service_id	  
+	WHERE ES.service_type = @service_type and SC.hospital_name = @hospital_name
+END;
+
+go
+--4.3.r
+CREATE PROCEDURE AdminViewCall
+     @service_type varchar(20) , @call_id INT
+AS
+BEGIN
+    SELECT  ES.service_type , CC.call_id
+	FROM  Call_Center CC INNER JOIN Emergency_Services ES ON CC.service_id = ES.service_id	  
+	WHERE ES.service_type = @service_type and CC.call_id = @call_id
+END;
+
+go
+--4.3.s
+CREATE PROCEDURE AdminViewCon
+     @ins_company varchar(20) , @hospital_name int
+AS
+BEGIN
+    SELECT  ins_company , hospital_name 
+	FROM  Insurance_signs
+	WHERE ins_company = @ins_company and hospital_name = @hospital_name
+END;
+go
+--4.3.t
+CREATE FUNCTION AdminViewCover (@TreatmentID INT)
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @Covered BIT
+
+    SELECT @Covered = covered
+    FROM Treatment
+    WHERE treatment_id = @TreatmentID;
+
+    RETURN @Covered;
+END;
+
+go
+--4.4.a
+create procedure checkHospitalsAndDoctors as
+select hospital_name,doctor_name from Doctor group by hospital_name;
+go
+--4.4.b
+create procedure checkDoctorsOfEachHospital (@hospital_name varchar(10)) as
+select hospital_name,doctor_name from Doctor d
+where d.hospital_name = @hospital_name group by d.hospital_name;
+go
+--4.4.c
+create function patientsMakeAppointment (@doctor_id int,@patient_id int) returns bit as
+begin
+declare @res bit
+if(exists(select doctor_id,patient_id from Appointment a 
+where @doctor_id = a.doctor_id and @patient_id=a.patient_id))set @res = 1 else set @res=0
+return @res
+end
+go
+--4.4.d
+create proc viewMyProfile @PatientId int output as
+select * from Patient p where p.patient_id = @PatientId;
+go
+--4.4.e
+create procedure editMyProfile (@patient_id int,
+@ehr varchar(30),
+@contact varchar(30),
+@allergies varchar(30),
+@diseases varchar(30),
+@vaccines varchar(30),
+@drugs varchar(30),
+@exam_result varchar(30),
+@nurse_id int,
+@ins_company varchar(20)) as
+if not (@ehr is null) update Patient set ehr = @ehr;
+if not (@contact is null) update Patient set contact = @contact;
+if not (@allergies is null) update Patient set allergies = @allergies;
+if not (@diseases is null) update Patient set diseases = @diseases;
+if not (@vaccines is null) update Patient set vaccines = @vaccines;
+if not (@drugs is null) update Patient set drugs = @drugs;
+if not (@exam_result is null) update Patient set exam_result = @exam_result;
+if not (@nurse_id is null) update Patient set nurse_id = @nurse_id;
+if not (@ins_company is null) update Patient set ins_company = @ins_company;
+
+go
+--4.5.a
+CREATE PROCEDURE DocViewHis
+    @PatientID INT
+AS
+BEGIN
+    SELECT * FROM Patient P
+    WHERE P.patient_id = @PatientID
+END;
+
+go
+--4.5.b
+CREATE FUNCTION DoctorReqLab (@DoctorID INT,@PatientID INT,@LabtestID INT)
+RETURNS BIT as BEGIN
+
+    DECLARE @Result BIT;
+    IF EXISTS (
+        SELECT Doctor D, Patient P, Labtest L 
+        WHERE (D.doctor_id = @DoctorID AND P.patient_id = @PatientID 
+            AND L.labtest_id = @LabtestID)) BEGIN SET @Result = 1; END
+    ELSE 
+        BEGIN SET @Result = 0; END
+   RETURN @Result;
+END;
+go
+--4.5.c
+CREATE FUNCTION DocWritePres
+    (@DoctorID INT,
+    @PatientID INT,
+    @Date DATE)
+
+RETURNS BIT
+AS 
+BEGIN
+
+    DECLARE @Result BIT;
+    IF EXISTS (
+        SELECT 
+            Prescription P
+        WHERE 
+            (P.doctor_id = @DoctorID 
+            AND 
+            P.patient_id = @PatientID
+            AND
+            P.prescription_date = @Date)
+    ) 
+    BEGIN SET @Result = 1; END
+    ELSE 
+        BEGIN SET @Result = 0; END
+
+    RETURN @Result;
+END;
+go
+--4.6.a
+create proc CheckPres @PharmacistID INT, @DoctorID INT, @PatientID INT,@Date Date as
+select wp.doctor_id,wp.prescription_date from
+Writes_Prescription wp inner join Pharmacist_checks pc 
+on wp.prescription_date = pc.prescription_date and wp.doctor_id = pc.doctor_id
+go
+--4.6.b
+create proc PharViewProfile @PharmacistID int as
+select * from Pharmacist ph where ph.pharmacist_id=@PharmacistID
+--4.6.c
+go
+create procedure UpdatePharProfile
+@pharmacistID int,
+@pharmacyName varchar(15),
+@startDate date,
+@password varchar(20),
+@pharmacistName varchar(20) as
+if not (@pharmacistID is null) update Pharmacist set ph_id = @pharmacistID;
+if not (@pharmacyName is null) update Pharmacist set pharmacy_name = @pharmacyName;
+if not (@startDate is null) update Pharmacist set startDate = @startDate;
+if not (@password is null) update Pharmacist set pass_word = @password;
+if not (@pharmacistName is null) update Pharmacist set pharmacist_name = @pharmacistName;
---
+++ tables_relations.sql
@@ -0,0 +1,292 @@
+﻿
+
+/*
+create table User_table(
+userId int,
+CONSTRAINT pk primary key(userId),
+user_type varchar(15),
+pass_word varchar(20)
+);
+
+
+create table Pharmacist(
+ph_id int,
+CONSTRAINT pk_pharmacist primary key(ph_id),
+pharmacy_name varchar(15),
+startDate date,
+pass_word varchar(20),
+pharmacist_name varchar(20),
+years_exp AS (YEAR(CURRENT_TIMESTAMP) - YEAR(startDate)),
+CONSTRAINT fk_pharmacist foreign key(ph_id) references User_table(userId) on update cascade on delete cascade
+);*/
+
+
+create table Hospital(
+hospital_name varchar(10),
+CONSTRAINT pk_hospital primary key(hospital_name),
+hospital_location varchar(20),
+email varchar(30),
+);
+
+create table Nurse(
+nurse_id int,
+CONSTRAINT pk_nurse primary key(nurse_id),
+first_name varchar(15),
+middle_name varchar(15),
+last_name varchar(15),
+nurst_shift varchar(15),
+hospital_name varchar(10),
+pass_word varchar(20),
+CONSTRAINT fk_nurse foreign key(nurse_id) references User_table(userId) on update cascade on delete cascade,
+CONSTRAINT fk_hospital_nurse foreign key(hospital_name) references Hospital(hospital_name) on update cascade on delete cascade
+);
+
+create table Insurance_Company(
+ins_name varchar(20),
+CONSTRAINT pk_ins primary key(ins_name)
+);
+
+create table Doctor(
+doctor_id int,
+CONSTRAINT pk_doctor primary key(doctor_id),
+working_hours int,
+working_days int,
+doctor_name varchar(15),
+specialization varchar(20),
+hospital_name varchar(10),
+pass_word varchar(20),
+CONSTRAINT fk_hospital_doctor foreign key(hospital_name) references Hospital(hospital_name) on update cascade on delete cascade,
+CONSTRAINT fk_user_doctor foreign key(doctor_id) references User_table(userId) on update cascade on delete cascade
+);
+create table Doctor_Numbers(
+doctor_id int,
+number varchar(20),
+CONSTRAINT pk_doctor_numbers primary key(doctor_id,number),
+CONSTRAINT fk_user_doctor foreign key(doctor_id) references User_table(userId) on update cascade on delete cascade
+);
+
+create table Patient(
+patient_id int,
+patient_name varchar(20),
+ehr varchar(30),
+contact varchar(30),
+allergies varchar(30),
+diseases varchar(30),
+vaccines varchar(30),
+drugs varchar(30),
+exam_result varchar(30),
+pass_word varchar(20),
+ins_company varchar(20),
+nurse_id int,
+CONSTRAINT pk_patient primary key(patient_id),
+CONSTRAINT fk_user_patient foreign key(patient_id) references User_table(userId) on update cascade on delete cascade,
+CONSTRAINT fk_nurse_patient foreign key(nurse_id) references Nurse(nurse_id) on update cascade on delete cascade,
+CONSTRAINT fk_ins_patient foreign key(ins_company) references Insurance_Company(ins_name) on update cascade on delete cascade,
+
+);
+create table Prescription(
+doctor_id int,
+prescription_date date,
+CONSTRAINT pk_prescription primary key(doctor_id,prescription_date),
+CONSTRAINT fk_doctor_prescription foreign key(doctor_id) references Doctor(doctor_id) on update cascade on delete cascade
+);
+
+create table Labtest(
+labtest_id int,
+patient_id int,
+CONSTRAINT pk_labtest primary key(labtest_id,patient_id),
+CONSTRAINT fk_patient_labtest foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+);
+
+go
+create function check_price(@price int) returns bit as
+begin 
+declare @res bit
+if @price < 200 set @res = 0 else set @res = 1
+return @res
+end;
+go
+create table Treatment(
+treatment_id int,
+price int,
+covered bit default (dbo.check_price(price)),
+patient_id int,
+ins_name varchar(20),
+CONSTRAINT pk_teatment primary key(treatment_id),
+CONSTRAINT fk_patient_treatment foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_ins_treatment foreign key(ins_company) references Insurance_Company(ins_name) on update cascade on delete cascade,
+);
+
+create table Medication(
+medication_id int,
+medication_type varchar(20),
+CONSTRAINT pk_medication primary key(medication_id),
+CONSTRAINT fk_treatment_medication foreign key(medication_id) references Treatment(treatment_id) on update cascade on delete cascade,
+);
+create table Surgery(
+surgery_id int,
+CONSTRAINT pk_surgery primary key(surgery_id),
+CONSTRAINT fk_treatment_surgery foreign key(surgery_id) references Treatment(treatment_id) on update cascade on delete cascade
+);
+
+create table Emergency_Services(
+service_id int,
+CONSTRAINT pk_emrgencyServices primary key(service_id),
+service_type varchar(20)
+);
+create table Call_Center(
+service_id int,
+call_id int,
+CONSTRAINT pk_callCenter primary key(service_id,call_id),
+CONSTRAINT fk_emrgencyServices_callCenter foreign key(service_id) references Emergency_Services(service_id) on update cascade on delete cascade
+);
+
+create table Diet_exercise(
+patient_id int,
+medication_id int,
+CONSTRAINT pk_diet primary key(patient_id,medication_id),
+CONSTRAINT fk_patient_diet foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_mediaction_diet foreign key(medication_id) references Medication(medication_id) on update cascade on delete cascade,
+);
+create table Takes_Medication(
+patient_id int,
+medication_id int,
+CONSTRAINT pk_patientAndTreatment primary key(patient_id,treatment_id),
+CONSTRAINT fk_patient_takesMedication foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_mediaction_takesMediaction foreign key(medication_id) references Medication(medication_id) on update cascade on delete cascade,
+);
+create table Appointment(
+patient_id int,
+doctor_id int,
+CONSTRAINT pk_patientAndDoctor primary key(patient_id,doctor_id),
+CONSTRAINT fk_patient_appointment foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_doctor_appointment foreign key(doctor_id) references Doctor(doctor_id) on update cascade on delete cascade,
+
+);
+create table Listen_examine(
+patient_id int,
+doctor_id int,
+CONSTRAINT pk_listen primary key(patient_id,doctor_id),
+CONSTRAINT fk_patient_listen foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_doctor_listen foreign key(doctor_id) references Doctor(doctor_id) on update cascade on delete cascade,
+
+);
+create table Diagnosis(
+patient_id int,
+doctor_id int,
+CONSTRAINT pk_diagnosis primary key(patient_id,doctor_id),
+CONSTRAINT fk1_patient_diagnosis foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk2_doctor_diagnosis foreign key(doctor_id) references Doctor(doctor_id) on update cascade on delete cascade,
+);
+create table View_History(
+patient_id int,
+doctor_id int,
+CONSTRAINT pk_view primary key(patient_id,doctor_id),
+CONSTRAINT fk_patient_view foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_doctor_view foreign key(doctor_id) references Doctor(doctor_id) on update cascade on delete cascade,
+);
+create table Checks(
+patient_id int,
+doctor_id int,
+hospital_name varchar(10),
+CONSTRAINT pk_checks primary key(patient_id,doctor_id,hospital_name),
+CONSTRAINT fk_patient_checks foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_doctor_checks foreign key(doctor_id) references Doctor(doctor_id) on update cascade on delete cascade,
+CONSTRAINT fk_hospital_checks foreign key(hospital_name) references Hospital(hospital_name) on update cascade on delete cascade,
+);
+
+create table Pharmacist_Medication(
+ph_id int,
+medication_id int,
+patient_id int,
+CONSTRAINT pk_pharmacistMedication primary key(patient_id,medication_id,ph_id),
+CONSTRAINT fk_pharmacist_pharmacistMedication foreign key(ph_id) references Pharmacist(ph_id) on update cascade on delete cascade,
+CONSTRAINT fk_mediaction_pharmacistMedication foreign key(medication_id) references Medication(medication_id) on update cascade on delete cascade,
+CONSTRAINT fk_patient_pharmacistMedication foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+);
+
+create table Pharmacist_checks_Prescription(
+ph_id int,
+prescription_date date,
+doctor_id int,
+patient_id int,
+CONSTRAINT pk_checksPrescription primary key(patient_id,prescription_date,doctor_id,ph_id),
+CONSTRAINT fk_pharmacist_checksPrescription foreign key(ph_id) references Pharmacist(ph_id) on update cascade on delete cascade,
+CONSTRAINT fk_prescription_checksPrescription foreign key(prescription_date,doctor_id) references Prescription(prescription_date,doctor_id) on update cascade on delete cascade,
+CONSTRAINT fk_patient_checksPrescription foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+);
+
+create table Services_Call(
+hospital_name varchar(10),
+service_id int,
+CONSTRAINT pk_serviceCall primary key(hospital_name,service_id),
+CONSTRAINT fk_hospital_serviceCall foreign key(hospital_name) references Hospital(hospital_name) on update cascade on delete cascade,
+CONSTRAINT fk_emergencyServices_serviceCall  foreign key(service_id) references Emergency_Services(service_id) on update cascade on delete cascade
+);
+
+create table Insurance_signs(
+ins_company varchar(20),
+hospital_name varchar(10),
+CONSTRAINT pk_insSigns primary key(hospital_name,ins_company),
+CONSTRAINT fk_hospital_insSigns foreign key(hospital_name) references Hospital(hospital_name) on update cascade on delete cascade,
+CONSTRAINT fk_ins_insSigns foreign key(ins_company) references Insurance_Company(ins_name) on update cascade on delete cascade,
+);
+
+create table Writes_Prescription(
+prescription_date date,
+doctor_id int,
+patient_id int,
+CONSTRAINT pk_writesPrescription primary key(prescription_date,patient_id,doctor_id),
+CONSTRAINT fk_patient_writesPrescription foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_doctor_writesPrescription foreign key(doctor_id) references Doctor(doctor_id) on update cascade on delete cascade,
+CONSTRAINT fk_prescription_writesPrescription foreign key (doctor_id, prescription_date) references Prescription (doctor_id, prescription_date) on update cascade on delete cascade
+);
+
+create table Facilities(
+patient_id int,
+hospital_name varchar(20),
+CONSTRAINT pk_facilities primary key(hospital_name,patient_id),
+CONSTRAINT fk_patient_facilities foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_hospital_facilities foreign key(hospital_name) references Hospital(hospital_name) on update cascade on delete cascade,
+);
+create table Admission(
+patient_id int,
+hospital_name varchar(20),
+CONSTRAINT pk_admission primary key(hospital_name,patient_id),
+CONSTRAINT fk_patient_admission foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_hospital_admission foreign key(hospital_name) references Hospital(hospital_name) on update cascade on delete cascade,
+);
+create table Hospital_Manages(
+patient_id int,
+hospital_name varchar(10),
+CONSTRAINT pk_hospitalManages primary key(hospital_name,patient_id),
+CONSTRAINT fk_patient_hospitalManages foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_hospital_hospitalManages foreign key(hospital_name) references Hospital(hospital_name) on update cascade on delete cascade,
+);
+
+create table Writes_Treatment(
+patient_id int,
+treatment_id int,
+doctor_id int,
+constraint pk_writesTreatment primary key(patient_id,treatment_id,doctor_id),
+CONSTRAINT fk_patient_writesTreatment foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_treatment_writesTreatment foreign key(treatment_id) references Treatment(treatment_id) on update cascade on delete cascade,
+CONSTRAINT fk_doctor_writesTreatment foreign key(doctor_id) references Doctor(doctor_id) on update cascade on delete cascade
+);
+create table Moniters(
+nurse_id int,
+patient_id int,
+treatment_id int,
+constraint pk_monitors primary key(patient_id,treatment_id,nurse_id),
+CONSTRAINT fk_patient_monitors foreign key(patient_id) references Patient(patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_treatment_monitors foreign key(treatment_id) references Treatment(treatment_id) on update cascade on delete cascade,
+CONSTRAINT fk_nurse_monitors foreign key(nurse_id) references Nurse(nurse_id) on update cascade on delete cascade
+);
+create table Requests_Labtest(
+patient_id int,
+labtest_id int,
+doctor_id int,
+constraint pk_requestsLabtest primary key(patient_id,treatment_id,doctor_id),
+CONSTRAINT fk_labtest_requestsLabtest foreign key(labtest_id,patient_id) references Labtest(labtest_id,patient_id) on update cascade on delete cascade,
+CONSTRAINT fk_doctor_requestsLabtest foreign key(doctor_id) references Doctor(doctor_id) on update cascade on delete cascade
+);
